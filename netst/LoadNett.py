e734a220a5de31bef1ccc516d94096474745026b
leandro-svg <leandro.dibella@gmail.com>
2022-05-31 12:14:00 +0200

    Adding files
    Notebook Data_Preprocess added

---
+++ netst/LoadNett.py
@@ -0,0 +1,16 @@
+
+from netst.ConvNett import ConvNett
+
+
+def convnet(net_params):
+
+    return
+
+
+def load_model(MODEL_NAME, net_params):
+
+    models = {
+            'ConvNet_': convnet(net_params)
+        }
+
+    return ConvNett(net_params)
